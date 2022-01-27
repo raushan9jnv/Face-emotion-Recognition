be8cae7fa993568508f6c488a8bcad2dcbde01dc
Raushan kumar <86125144+raushan9jnv@users.noreply.github.com>
2022-01-27 16:16:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
-port = var port = process.env.PORT || 8051;
+port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
